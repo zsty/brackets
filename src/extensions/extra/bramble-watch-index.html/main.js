3c61da1b329923b116ef4e7d1e33d05fbbd327a4
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2016-11-08 10:13:01 -0500

    Review fixes

--- src/extensions/extra/bramble-watch-index.html/main.js
+++ src/extensions/extra/bramble-watch-index.html/main.js
@@ -1,5 +1,5 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
-/*global define, brackets, clearTimeout, setTimeout */
+/*global define, brackets */
 
 define(function (require, exports, module) {
     "use strict";
