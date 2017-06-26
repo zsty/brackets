6ee9136cb152925c56496cffa8d6ac516f3a7ec0
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2017-06-26 13:36:26 -0400

    Clean up PDFView, basically done now

--- src/extensions/default/PDFView/PDFView.js
+++ src/extensions/default/PDFView/PDFView.js
@@ -24,21 +24,9 @@
 define(function (require, exports, module) {
     "use strict";
 
-    var DocumentManager     = brackets.getModule("document/DocumentManager"),
-        ProjectManager      = brackets.getModule("project/ProjectManager"),
-        Strings             = brackets.getModule("strings"),
-        StringUtils         = brackets.getModule("utils/StringUtils"),
-        FileSystem          = brackets.getModule("filesystem/FileSystem"),
-        UrlCache            = brackets.getModule("filesystem/impls/filer/UrlCache"),
-        FileUtils           = brackets.getModule("file/FileUtils"),
-        _                   = brackets.getModule("thirdparty/lodash"),
+    var UrlCache            = brackets.getModule("filesystem/impls/filer/UrlCache"),
         Mustache            = brackets.getModule("thirdparty/mustache/mustache"),
         PDFViewTemplate     = require("text!htmlContent/pdf-view.html");
-
-    // Get a URL out of the cache
-    function _getPDFUrl(path) {
-        return encodeURIComponent(UrlCache.getUrl(path));
-    }
 
     /**
      * PDFView objects are constructed when a PDF file is opened
@@ -50,62 +38,14 @@ define(function (require, exports, module) {
      */
     function PDFView(file, $container) {
         this.file = file;
-        var fullPath = file.fullPath;
-        this.relPath = ProjectManager.makeProjectRelativeIfPossible(fullPath);
-
         this.$container = $container;
+
         this.$el = $(Mustache.render(PDFViewTemplate, {
-            pdfUrl: _getPDFUrl(fullPath),
+            pdfUrl: encodeURIComponent(UrlCache.getUrl(file.fullPath)),
             locale: brackets.getLocale()
         }));
         $container.append(this.$el);
-
-//        this.$imagePath = this.$el.find(".image-path");
-//        this.$imagePreview = this.$el.find(".image-preview");
-//        this.$imageData = this.$el.find(".image-data");
-//
-//        this.$image = this.$el.find(".image");
-//        this.$imageScale = this.$el.find(".image-scale");
-//        this.$imagePreview.on("load", _.bind(this._onImageLoaded, this));
-//        this.$imagePreview.on("error", _.bind(console.error, console));
     }
-
-    /**
-     * DocumentManger.fileNameChange handler - when an image is renamed, we must
-     * update the view
-     *
-     * @param {jQuery.Event} e - event
-     * @param {!string} oldPath - the name of the file that's changing changing
-     * @param {!string} newPath - the name of the file that's changing changing
-     * @private
-     */
-    PDFView.prototype._onFilenameChange = function (e, oldPath, newPath) {
-        /*
-         * File objects are already updated when the event is triggered
-         * so we just need to see if the file has the same path as our image
-         */
-        if (this.file.fullPath === newPath) {
-            this.relPath = ProjectManager.makeProjectRelativeIfPossible(newPath);
-        }
-    };
-
-    /**
-     * <img>.on("load") handler - updates content of the image view
-     *                            initializes computed values
-     *                            installs event handlers
-     * @param {Event} e - event
-     * @private
-     */
-    PDFView.prototype._onImageLoaded = function (e) {
-        // TODO: figure this out...
-
-        // make sure we always show the right file name
-        //DocumentManager.on("fileNameChange.PDFView", _.bind(this._onFilenameChange, this));
-    };
-
-    /**
-     * View Interface functions
-     */
 
     /*
      * Retrieves the file object for this view
@@ -127,8 +67,8 @@ define(function (require, exports, module) {
             oWidth = $container.outerWidth(),
             oHeight = $container.outerHeight();
 
-        // $view is "position:absolute" so
-        //  we have to update the height, width and position
+        // view is "position:absolute" so
+        // we have to update the height, width and position
         this.$el.css({
             top: pos.top + ((oHeight - iHeight) / 2),
             left: pos.left + ((oWidth - iWidth) / 2),
@@ -145,16 +85,6 @@ define(function (require, exports, module) {
     };
 
     /*
-     * Refreshes the image preview with what's on disk
-     */
-    PDFView.prototype.refresh = function () {
-        console.log("refresh");
-        // Update the DOM node with the src URL
-        // TODO
-        //this.$imagePreview.attr("src", _getPDFUrl(this.file));
-    };
-
-    /*
      * Creates a PDF view object and adds it to the specified pane
      * @param {!File} file - the file to create an image of
      * @param {!Pane} pane - the pane in which to host the view
@@ -162,47 +92,16 @@ define(function (require, exports, module) {
      */
     function create(file, pane) {
         var view = pane.getViewForPath(file.fullPath);
+
         if (view) {
-            console.log("found view, showing", file);
             pane.showView(view);
         } else {
-            console.log("no view found, creating", file);
             view = new PDFView(file, pane.$content);
             pane.addView(view, true);
         }
 
         return new $.Deferred().resolve(view.getFile()).promise();
     }
-
-    /**
-     * Handles file system change events so we can refresh
-     *  image viewers for the files that changed on disk due to external editors
-     * @param {jQuery.event} event - event object
-     * @param {?File} file - file object that changed
-     * @param {Array.<FileSystemEntry>=} added If entry is a Directory, contains zero or more added children
-     * @param {Array.<FileSystemEntry>=} removed If entry is a Directory, contains zero or more removed children
-     */
-    function _handleFileSystemChange(event, entry, added, removed) {
-        // this may have been called because files were added
-        //  or removed to the file system.  We don't care about those
-        if (!entry || entry.isDirectory) {
-            return;
-        }
-
-        // Look for a viewer for the changed file
-        var viewer = _viewers[entry.fullPath];
-
-        // viewer found, call its refresh method
-        if (viewer) {
-            viewer.refresh();
-        }
-    }
-
-    /*
-     * Install an event listener to receive all file system change events
-     * so we can refresh the view when changes are made to the image in an external editor
-     */
-    FileSystem.on("change", _handleFileSystemChange);
 
     exports.create = create;
 });
