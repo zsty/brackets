fbc35d4acb0c7cfe7b7215fff57d0007c661e2ca
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2017-07-20 16:57:29 -0400

    Move to extensions/extra

--- src/utils/BrambleExtensionLoader.js
+++ src/utils/BrambleExtensionLoader.js
@@ -16,70 +16,7 @@ define(function (require, exports, module) {
     // Load the list of extensions. If you want to add/remove extensions, do it in this json file.
     var extensionInfo = JSON.parse(require("text!extensions/bramble-extensions.json"));
 
-<<<<<<< HEAD
     // Disable any extensions we found on the query string's ?disableExtensions= param
-=======
-    /**
-     * We have a set of defaults we load if not instructed to do otherwise
-     * via the disableExtensions query param. These live in src/extensions/default/*
-     */
-    var brambleDefaultExtensions = [
-        "CSSCodeHints",
-        "HTMLCodeHints",
-        "JavaScriptCodeHints",
-        "InlineColorEditor",
-        "JavaScriptQuickEdit",
-        "QuickOpenCSS",
-        "QuickOpenHTML",
-        "QuickOpenJavaScript",
-        "QuickView",
-        "WebPlatformDocs",
-        "CodeFolding",
-
-        // Custom extensions we want loaded by default
-        "bramble",
-        "Autosave",
-        "brackets-paste-and-indent",
-        "BrambleUrlCodeHints",
-        "UploadFiles",
-        "bramble-move-file",
-        "Brackets-InteractiveLinter"
-    ];
-
-    /**
-     * There are some Brackets default extensions that we don't load
-     * but which a user might want to enable (note: not all the defaults
-     * they ship will work in a browser, so they aren't all here). We
-     * support loading these below via the enableExtensions param.
-     */
-    var bracketsDefaultExtensions = [
-        "SVGCodeHints",
-        "HtmlEntityCodeHints",
-        "LESSSupport",
-        "CloseOthers",
-        "InlineTimingFunctionEditor",
-        "JSLint",
-        "QuickOpenCSS",
-        "RecentProjects",
-        "UrlCodeHints"
-    ];
-
-    /**
-     * Other extensions we've tested and deemed useful in the Bramble context.
-     * These live in src/extensions/extra/* and are usually submodules.  If you
-     * add a new extension there, update this array also.  You can have this load
-     * by adding the extension name to the enableExtensions query param.
-     */
-    var extraExtensions = [
-        "brackets-cdn-suggestions",    // https://github.com/szdc/brackets-cdn-suggestions
-        "HTMLHinter",
-        "MDNDocs",
-        "SVGasXML",
-        "bramble-watch-index.html"
-    ];
-
-    // Disable any extensions we found on the query string's disableExtensions param
->>>>>>> Rework https://github.com/MiguelCastillo/Brackets-InteractiveLinter to load in browser
     function _processDefaults(disableExtensions) {
         disableExtensions = disableExtensions ? disableExtensions.trim().split(/\s*,\s*/) : [];
 
